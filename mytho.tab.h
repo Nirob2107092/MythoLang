7af0cf7c873fa45e6dbb62729035c2daf249ce43
Nirob2107092 <perfeccnirob@gmail.com>
2026-03-09 13:01:11 +0600

    added bison parser for mytholand syntax analysis

---
+++ mytho.tab.h
@@ -0,0 +1,154 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_MYTHO_TAB_H_INCLUDED
+# define YY_YY_MYTHO_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    KEYWORD_INT = 258,             /* KEYWORD_INT  */
+    KEYWORD_FLOAT = 259,           /* KEYWORD_FLOAT  */
+    KEYWORD_DOUBLE = 260,          /* KEYWORD_DOUBLE  */
+    KEYWORD_LONG = 261,            /* KEYWORD_LONG  */
+    KEYWORD_CHAR = 262,            /* KEYWORD_CHAR  */
+    KEYWORD_BOOL = 263,            /* KEYWORD_BOOL  */
+    KEYWORD_VOID = 264,            /* KEYWORD_VOID  */
+    IF = 265,                      /* IF  */
+    ELSE_IF = 266,                 /* ELSE_IF  */
+    ELSE = 267,                    /* ELSE  */
+    FOR = 268,                     /* FOR  */
+    WHILE = 269,                   /* WHILE  */
+    DO = 270,                      /* DO  */
+    SWITCH = 271,                  /* SWITCH  */
+    CASE = 272,                    /* CASE  */
+    DEFAULT = 273,                 /* DEFAULT  */
+    BREAK = 274,                   /* BREAK  */
+    CONTINUE = 275,                /* CONTINUE  */
+    RETURN = 276,                  /* RETURN  */
+    FUNCTION = 277,                /* FUNCTION  */
+    MAIN = 278,                    /* MAIN  */
+    PRINT = 279,                   /* PRINT  */
+    INPUT = 280,                   /* INPUT  */
+    ASSIGN = 281,                  /* ASSIGN  */
+    RETTYPE = 282,                 /* RETTYPE  */
+    OP_ADD = 283,                  /* OP_ADD  */
+    OP_SUB = 284,                  /* OP_SUB  */
+    OP_MUL = 285,                  /* OP_MUL  */
+    OP_DIV = 286,                  /* OP_DIV  */
+    OP_MOD = 287,                  /* OP_MOD  */
+    OP_AND = 288,                  /* OP_AND  */
+    OP_OR = 289,                   /* OP_OR  */
+    OP_NOT = 290,                  /* OP_NOT  */
+    OP_XOR = 291,                  /* OP_XOR  */
+    OP_POW = 292,                  /* OP_POW  */
+    OP_LT = 293,                   /* OP_LT  */
+    OP_GT = 294,                   /* OP_GT  */
+    OP_LE = 295,                   /* OP_LE  */
+    OP_GE = 296,                   /* OP_GE  */
+    OP_EQ = 297,                   /* OP_EQ  */
+    OP_NE = 298,                   /* OP_NE  */
+    ROOT = 299,                    /* ROOT  */
+    FLOOR_FN = 300,                /* FLOOR_FN  */
+    CEIL_FN = 301,                 /* CEIL_FN  */
+    ABS_FN = 302,                  /* ABS_FN  */
+    LOG_FN = 303,                  /* LOG_FN  */
+    SIN_FN = 304,                  /* SIN_FN  */
+    COS_FN = 305,                  /* COS_FN  */
+    TAN_FN = 306,                  /* TAN_FN  */
+    ASIN_FN = 307,                 /* ASIN_FN  */
+    ACOS_FN = 308,                 /* ACOS_FN  */
+    ATAN_FN = 309,                 /* ATAN_FN  */
+    AMBROSIA_FN = 310,             /* AMBROSIA_FN  */
+    LPAREN = 311,                  /* LPAREN  */
+    RPAREN = 312,                  /* RPAREN  */
+    LBRACE = 313,                  /* LBRACE  */
+    RBRACE = 314,                  /* RBRACE  */
+    DOT = 315,                     /* DOT  */
+    COLON = 316,                   /* COLON  */
+    COMMA = 317,                   /* COMMA  */
+    INVALID = 318,                 /* INVALID  */
+    INT_LITERAL = 319,             /* INT_LITERAL  */
+    BOOL_LITERAL = 320,            /* BOOL_LITERAL  */
+    FLOAT_LITERAL = 321,           /* FLOAT_LITERAL  */
+    CHAR_LITERAL = 322,            /* CHAR_LITERAL  */
+    IDENTIFIER = 323,              /* IDENTIFIER  */
+    STRING_LITERAL = 324           /* STRING_LITERAL  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 14 "mytho.y"
+
+    int ival;
+    double fval;
+    char cval;
+    char* sval;
+
+#line 140 "mytho.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_MYTHO_TAB_H_INCLUDED  */
---
+++ mytho.y
@@ -0,0 +1,136 @@
+%{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int yylex(void);
+void yyerror(const char *s);
+
+extern int yylineno;
+extern FILE *yyin;
+FILE *outputFile;
+%}
+
+%union {
+    int ival;
+    double fval;
+    char cval;
+    char* sval;
+}
+
+/* tokens from lexer */
+%token KEYWORD_INT KEYWORD_FLOAT KEYWORD_DOUBLE KEYWORD_LONG KEYWORD_CHAR KEYWORD_BOOL KEYWORD_VOID
+%token IF ELSE_IF ELSE FOR WHILE DO SWITCH CASE DEFAULT BREAK CONTINUE RETURN
+%token FUNCTION MAIN PRINT INPUT
+%token ASSIGN RETTYPE
+%token OP_ADD OP_SUB OP_MUL OP_DIV OP_MOD
+%token OP_AND OP_OR OP_NOT OP_XOR OP_POW
+%token OP_LT OP_GT OP_LE OP_GE OP_EQ OP_NE
+%token ROOT FLOOR_FN CEIL_FN ABS_FN LOG_FN SIN_FN COS_FN TAN_FN
+%token ASIN_FN ACOS_FN ATAN_FN AMBROSIA_FN
+%token LPAREN RPAREN LBRACE RBRACE DOT COLON COMMA
+%token INVALID
+
+%token <ival> INT_LITERAL BOOL_LITERAL
+%token <fval> FLOAT_LITERAL
+%token <cval> CHAR_LITERAL
+%token <sval> IDENTIFIER STRING_LITERAL
+
+/* precedence */
+%left OP_ADD OP_SUB
+%left OP_MUL OP_DIV OP_MOD
+%right OP_POW
+
+/* expression value types */
+%type <ival> expression
+
+%%
+
+program
+    : main_function
+      {
+          fprintf(outputFile, "Parsing Successful\n");
+      }
+    ;
+
+main_function
+    : MAIN LPAREN RPAREN LBRACE statement_list RBRACE
+    ;
+
+statement_list
+    : statement_list statement
+    | /* empty */
+    ;
+
+statement
+    : declaration DOT
+    | assignment DOT
+    | print_stmt DOT
+    ;
+
+declaration
+    : type_spec IDENTIFIER
+    | type_spec IDENTIFIER ASSIGN expression
+    ;
+
+assignment
+    : IDENTIFIER ASSIGN expression
+    ;
+
+print_stmt
+    : PRINT LPAREN expression RPAREN
+    ;
+
+type_spec
+    : KEYWORD_INT
+    | KEYWORD_FLOAT
+    | KEYWORD_DOUBLE
+    | KEYWORD_LONG
+    | KEYWORD_CHAR
+    | KEYWORD_BOOL
+    ;
+
+expression
+    : expression OP_ADD expression   { $$ = $1 + $3; }
+    | expression OP_SUB expression   { $$ = $1 - $3; }
+    | expression OP_MUL expression   { $$ = $1 * $3; }
+    | expression OP_DIV expression   { 
+                                        if ($3 == 0) {
+                                            yyerror("division by zero");
+                                            $$ = 0;
+                                        } else {
+                                            $$ = $1 / $3;
+                                        }
+                                      }
+    | expression OP_MOD expression   { $$ = $1 % $3; }
+    | LPAREN expression RPAREN       { $$ = $2; }
+    | INT_LITERAL                    { $$ = $1; }
+    | IDENTIFIER                     { $$ = 0; } 
+    ;
+
+%%
+
+void yyerror(const char *s) {
+    fprintf(outputFile, "Syntax Error at line %d\n", yylineno);
+}
+
+int main(void) {
+    yyin = fopen("input_mytho.txt", "r");
+    if (!yyin) {
+        perror("input_mytho.txt");
+        return 1;
+    }
+
+    outputFile = fopen("output.txt", "w");
+    if (!outputFile) {
+        perror("output.txt");
+        return 1;
+    }
+
+    yyparse();
+
+    fclose(yyin);
+    fclose(outputFile);
+
+    return 0;
+}
